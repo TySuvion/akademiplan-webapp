36f88cf1fe149f9343b8f87cb377068cae6066b7
TySuvion <80416049+TySuvion@users.noreply.github.com>
2025-05-10 14:25:05 +0200

    login from redone with material design

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -8,10 +8,25 @@ import {
 import { RouterLink } from '@angular/router';
 import { ApiService } from '../../services/api.service';
 import { Router } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [ReactiveFormsModule, RouterLink],
+  imports: [
+    ReactiveFormsModule,
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    RouterLink,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
