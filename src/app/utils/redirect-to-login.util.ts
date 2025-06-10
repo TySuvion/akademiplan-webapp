dc709e6c5bb1488b9d5ff1a4ccc4711bd466a9b3
TySuvion <80416049+TySuvion@users.noreply.github.com>
2025-06-10 20:10:44 +0200

    fixed the storage is not defined issue

--- src/app/utils/redirect-to-login.util.ts
+++ src/app/utils/redirect-to-login.util.ts
@@ -1,7 +1,10 @@
 import { Router } from '@angular/router';
 
 export function redirectToLogin(router: Router) {
-  localStorage.clear();
-  console.log('Redirecting to login');
-  router.navigate(['/login']);
+  try {
+    localStorage.clear();
+    router.navigate(['/login']);
+  } catch (error) {
+    router.navigate(['/login']);
+  }
 }
